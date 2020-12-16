630b3ce061134ca7ab903fb4c0a27f636c517fb2
RussellSB <r.sammutbonnici@gmail.com>
2020-12-16 11:41:26 +0100

    Adjusted running params

--- src/Run.java
+++ src/Run.java
@@ -40,7 +40,7 @@ public class Run {
 
         //default
         if(args.length == 0)
-            args = new String[]{"0", "5", "5", "-1", "6", "8", "5", "4"};
+            args = new String[]{"0", "10", "5", "-1", "6", "8", "5", "4"};
 
         if(args.length != 8) {
             printHelp();
